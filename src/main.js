9a2d9da895377ff28aeb731905e9f31a933f63c4
Cameron Marcus <cammarcus19@gmail.com>
2024-01-08 22:30:26 -0500

    api call works

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import './App.css';
 
 
 function Main() {
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    const getGroops = async () => {
+        const getUrl = apiUrl;
+        const response = await fetch(apiUrl, {
+            method: 'GET'
+        })
+        console.log(response)
+        const data = await response.json();
+        console.log('Data:', data);
+        
+
+    };
+
     return (
         <div className="App">
             <header className="App-header">
@@ -15,6 +29,7 @@ function Main() {
                 >
                     Learn React
                 </a>
+                <button onClick={getGroops}>button here</button>
                 <p className='flex items-center'>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
