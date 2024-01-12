0aca8bf7ab11390f75dbbdabd22d3bccbc78e9a5
Cameron Marcus <cammarcus19@gmail.com>
2024-01-11 23:35:28 -0500

    tile click functionality added

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,13 +1,35 @@
+import { useState } from 'react';
 
+function Tile({tilename, groupNum, selectedTiles, setSelectedTiles}) {
 
-function Tile({tilename}) {
+    const[clicked, setClicked] = useState(false);
+
+    const tileClicked = async () => {
+        if (clicked) {
+            setSelectedTiles(selectedTiles => selectedTiles.filter(item => item !== tilename));
+            setClicked(!clicked)
+        } else {
+            if (selectedTiles.length < 4) {
+                setSelectedTiles(selectedTiles => selectedTiles.concat(tilename));
+                setClicked(!clicked)
+            } else {
+                console.log('we cant add')
+            }
+        }
+    }
 
 
     return (
-        <button>
-            <div className="grid-item p-4 bg-neutral-200">
-                {tilename}
-            </div>
+        <button onClick={tileClicked}>
+            {clicked ? (
+                <div className="grid-item p-4 bg-neutral-500">
+                    {tilename}
+                </div>
+            ) : (
+                <div className="grid-item p-4 bg-neutral-200">
+                    {tilename}
+                </div>
+            )}
         </button>
     );
 }
--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import Tile from './components/tile';
 
 function Main() {
     const apiUrl = 'https://ts2pxvn89b.execute-api.us-east-1.amazonaws.com/items/';
-    const [groops, setGroops] = useState({"hi":0, "why": 1, "bye":2, "arg":3, "h":0, "whyf": 1, "hiff":2, "whffy":3, "hffi":0, "whdy": 1, "hdsdi":2, "whdsfdy":3, "hvvi":0, "whyd": 1, "hiddd":2, "whfdfy":3});
-    const [groopOneName, setGroopOneName] = useState('')
-    const [groopTwoName, setGroopTwoName] = useState('')
-    const [groopThreeName, setGroopThreeName] = useState('')
-    const [groopFourName, setGroopFourName] = useState('')
+    const [groops, setGroops] = useState({});
+    const [groopOneName, setGroopOneName] = useState('');
+    const [groopTwoName, setGroopTwoName] = useState('');
+    const [groopThreeName, setGroopThreeName] = useState('');
+    const [groopFourName, setGroopFourName] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [selectedTiles, setSelectedTiles] = useState([])
 
     const getGroops = async (groopIdInput) => {
+        setLoading(true);
         let groopId = groopIdInput;
         groopId = groopId.toString()
         const getUrl = apiUrl + groopId;
@@ -24,7 +27,7 @@ function Main() {
         let groop_two_values_array = groops_data['groop_two_values']['SS']
         let groop_three_values_array = groops_data['groop_three_values']['SS']
         let groop_four_values_array = groops_data['groop_four_values']['SS']
-        let newGroops = { };
+        let newGroops = {};
         for (let i = 0; i < groop_one_values_array.length; i++) {
             let currVal = groop_one_values_array[i]
             newGroops = { ...newGroops, [currVal]: 1 };
@@ -46,27 +49,46 @@ function Main() {
         setGroopTwoName(groops_data['groop_two_name']['S'])
         setGroopThreeName(groops_data['groop_three_name']['S'])
         setGroopFourName(groops_data['groop_four_name']['S'])
-        setGroops(newGroops)
+        setGroops(newGroops);
+        setLoading(false);
     };
 
-    useEffect(()=> {
+    const enter = async () => {
+        console.log('enter clicked')
+
+        //console.log('correct')
+        //console.log('one away')
+        //console.log('nope')
+    }
+
+    useEffect(() => {
         let groopIdInput = 34;
         getGroops(groopIdInput);
-    },[])
+
+    }, [])
 
     return (
         <div className="App">
             <p>Groops</p>
-            <div className="grid grid-cols-4 gap-4" id="tile-container">
-                {Object.entries(groops).map(([key, value]) => (
-                    <div key={key}>
-                        <Tile tilename={key}></Tile>
+            <div>
+                {!loading ? (
+                <div className="grid grid-cols-4 gap-4" id="tile-container">
+                    {Object.entries(groops).map(([key, value]) => (
+                        <div key={key}>
+                            <Tile tilename={key} groopNum={value} selectedTiles={selectedTiles} setSelectedTiles={setSelectedTiles}></Tile>
+                        </div>
+                    ))}
+                </div>
+                ) : (
+                    <div>
+                        loading
                     </div>
-                ))}
+                )
+                }
             </div>
             <button>shuffle</button>
             <button>new set</button>
-            <button>enter</button>
+            <button onClick={enter}>enter</button>
         </div>
     );
 }
