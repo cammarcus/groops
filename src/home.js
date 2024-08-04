9f404a6b14ed162d26ff413aeb5cdbbb54b52e1f
Cameron Marcus <cammarcus19@gmail.com>
2024-08-04 11:53:02 -0400

    updated home screen

--- src/home.js
+++ src/home.js
@@ -5,100 +5,81 @@ function Home() {
     const navigate = useNavigate();
     const getUrl = 'https://ts2pxvn89b.execute-api.us-east-1.amazonaws.com/items';
     const [userEnteredID, setUserEnteredID] = useState('');
-    const [isHoveredClickToPlay, setIsHoveredClickToPlay] = useState(false);
-    const [isHoveredClickToSubmit, setIsHoveredClickToSubmit] = useState(false);
-    const [isHoveredClickToCreate, setIsHoveredClickToCreate] = useState(false);
+    const [isHovered, setIsHovered] = useState({
+        clickToPlay: false,
+        clickToSubmit: false,
+        clickToCreate: false,
+    });
 
-    const handleHoverClickToPlay = () => {
-        setIsHoveredClickToPlay(true);
+    const handleMouseEnter = (button) => {
+        setIsHovered((prev) => ({ ...prev, [button]: true }));
     };
 
-    const handleLeaveClickToPlay = () => {
-        setIsHoveredClickToPlay(false);
+    const handleMouseLeave = (button) => {
+        setIsHovered((prev) => ({ ...prev, [button]: false }));
     };
-
-    const handleHoverClickToSubmit = () => {
-        setIsHoveredClickToSubmit(true);
-    };
-
-    const handleLeaveClickToSubmit = () => {
-        setIsHoveredClickToSubmit(false);
-    };
-
-    const handleHoverClickToCreate = () => {
-        setIsHoveredClickToCreate(true);
-    };
-
-    const handleLeaveClickToCreate = () => {
-        setIsHoveredClickToCreate(false);
-    };
-
 
     const rerouteToRandomID = async () => {
         let rerouteId = '/error';
-        const response = await fetch(getUrl, {
-            method: 'GET'
-        });
-        const data = await response.json();
-        rerouteId = '/' + data.toString();
+        try {
+            const response = await fetch(getUrl, { method: 'GET' });
+            const data = await response.json();
+            rerouteId = `/${data.toString()}`;
+        } catch (error) {
+            console.error('Error fetching random ID:', error);
+        }
         navigate(rerouteId);
     };
-    const rerouteWithEnteredID = async () => {
+
+    const rerouteWithEnteredID = () => {
         navigate(`/${userEnteredID}`);
     };
-    const rerouteToCreate = async () => {
-        navigate(`/create`);
+
+    const rerouteToCreate = () => {
+        navigate('/create');
     };
 
     return (
         <div className="flex flex-col items-center justify-center h-screen p-4 bg-gray-100 text-gray-900">
-            <div className=''>
-                <p className="text-center text-4xl font-serif font-bold mb-4">
-                    Welcome to Groops
-                </p>
-            </div>
-            <div onMouseEnter={handleHoverClickToPlay}
-                onMouseLeave={handleLeaveClickToPlay}
-                className='mb-4'>
-                <button
-                    className="px-8 py-3 rounded-md text-white font-semibold hover:text-white transition duration-300"
-                    onClick={rerouteToRandomID}
-                    style={{ backgroundColor: isHoveredClickToPlay ? '#C9D7F8' : '#D4C5E2' }}
-                >
-                    Click to play
-                </button>
-            </div>
-            <div className='flex flex-col items-center mb-4'>
-                <p className="text-2xl font-serif font-bold mb-4">Have an id?</p>
+            <h1 className="text-center text-4xl font-serif font-bold mb-8">Welcome to Groops</h1>
+
+            <button
+                className="w-64 mb-12 px-8 py-3 rounded-full text-white font-semibold transition-transform transform hover:scale-105 shadow-lg"
+                onClick={rerouteToRandomID}
+                onMouseEnter={() => handleMouseEnter('clickToPlay')}
+                onMouseLeave={() => handleMouseLeave('clickToPlay')}
+                style={{ backgroundColor: isHovered.clickToPlay ? '#C9D7F8' : '#D4C5E2' }}
+            >
+                Click to Play
+            </button>
+
+            <button
+                className="w-64 mb-12 px-8 py-3 rounded-full text-white font-semibold transition-transform transform hover:scale-105 shadow-lg"
+                onClick={rerouteToCreate}
+                onMouseEnter={() => handleMouseEnter('clickToCreate')}
+                onMouseLeave={() => handleMouseLeave('clickToCreate')}
+                style={{ backgroundColor: isHovered.clickToCreate ? '#C9D7F8' : '#D4C5E2' }}
+            >
+                Create Your Own
+            </button>
+
+            <div className="flex flex-col items-center mt-12">
+                <p className="text-xl font-serif font-bold mb-4">Have an ID?</p>
                 <input
-                    title="Enter id"
-                    className="p-2 border border-gray-300 rounded-md mb-4 bg-gray-100"
+                    title="Enter ID"
+                    className="w-64 p-2 border border-gray-300 rounded-md mb-4 bg-gray-100 focus:ring-2 focus:ring-purple-500"
                     value={userEnteredID}
                     onChange={(e) => setUserEnteredID(e.target.value)}
                 />
-                <div onMouseEnter={handleHoverClickToSubmit}
-                    onMouseLeave={handleLeaveClickToSubmit}>
-                    <button
-                        className="px-8 py-3 rounded-md text-white font-semibold hover:text-white transition duration-300"
-                        onClick={rerouteWithEnteredID}
-                        style={{ backgroundColor: isHoveredClickToSubmit ? '#C9D7F8' : '#D4C5E2' }}
-                    >
-                        Submit
-                    </button>
-                </div>
-            </div>
-            <div className='flex flex-col items-center mb-4'>
-                <p className="text-2xl font-serif font-bold mb-4">Or</p>
-                <div onMouseEnter={handleHoverClickToCreate}
-                    onMouseLeave={handleLeaveClickToCreate}>
-                    <button
-                        className="px-8 py-3 rounded-md text-white font-semibold hover:text-white transition duration-300"
-                        onClick={rerouteToCreate}
-                        style={{ backgroundColor: isHoveredClickToCreate ? '#C9D7F8' : '#D4C5E2' }}
-                    >
-                        Create your own
-                    </button>
-                </div>
+                <button
+                    className="w-64 px-8 py-3 rounded-full text-white font-semibold transition-transform transform hover:scale-105 shadow-lg"
+                    onClick={rerouteWithEnteredID}
+                    onMouseEnter={() => handleMouseEnter('clickToSubmit')}
+                    onMouseLeave={() => handleMouseLeave('clickToSubmit')}
+                    style={{ backgroundColor: isHovered.clickToSubmit ? '#C9D7F8' : '#D4C5E2' }}
+                >
+                    Submit
+                </button>
             </div>
         </div>
     );
